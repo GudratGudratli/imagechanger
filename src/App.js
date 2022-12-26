b2bf654831afe9e0d36f3b845c7ecc8a43cd555d
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-12-27 01:05:47 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const handleClick = (event, message) => {
+    // const selectedImg = useRef("img")
+    console.log(event.target);
+    console.log(message);
+    console.log('clicked');
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1590273466070-40c466b4432d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW4lMjBmb3Jlc3R8ZW58MHx8MHx8&w=1000&q=80"
+        alt="forest"
+        onClick={
+          (event) => handleClick(event, 'hello World')
+        }
+      />
     </div>
   );
-}
-
+};
 export default App;
